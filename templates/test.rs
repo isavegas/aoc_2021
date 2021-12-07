146e0c353c23c6691da520c3957c18d71928ef7d
Kenzi Jeanis <isavegas2006@gmail.com>
2021-12-07 01:40:36 -0600

    Add templates

---
+++ templates/test.rs
@@ -0,0 +1,23 @@
+use aoc_2021::day::day_@DAY@::get_day;
+
+const INPUT: &str = r#""#;
+
+#[test]
+pub fn part1_1() {{
+    assert_eq!(
+        get_day()
+            .part1(INPUT)
+            .expect("Error"),
+        "".to_string()
+    );
+}}
+
+#[test]
+pub fn part2_1() {{
+    assert_eq!(
+        get_day()
+            .part2(INPUT)
+            .expect("Error"),
+        "".to_string()
+    );
+}}
